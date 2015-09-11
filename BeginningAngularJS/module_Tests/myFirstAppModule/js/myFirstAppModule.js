9df4a20ae8af11c266fd22f8f001af28dca6d74d
Zizazao <zizamv@gmail.com>
2015-09-11 16:58:51 +0200

    controller fixed

--- BeginningAngularJS/module_Tests/myFirstAppModule/js/myFirstAppModule.js
+++ BeginningAngularJS/module_Tests/myFirstAppModule/js/myFirstAppModule.js
@@ -1,7 +1,15 @@
 var myFirstAppModule = angular.module('myFirstAppModule', [])
 
 
-myFirstAppModule.controller('MyFilterDemoCtrl', function ($scope) {       
+myFirstAppModule.controller('MyFilterDemoCtrl', function ($scope) {
+	var someData = {            
+	   	firstName: 'JENNA',            
+		surname: 'GRANT',            
+		dateJoined: new Date(2010, 2, 23),            
+		consumption: 123.659855,            
+		plan: 'super-basic-plan'        
+		}; 
+        $scope.data = someData;        
 
 	} 
 );
